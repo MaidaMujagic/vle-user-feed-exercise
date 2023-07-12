bc7631ccf02ca3e6507e2ec679fa64a22264c3a4
Maida Mujagic <maida.mujagic@york.ac.uk>
2023-07-12 11:47:09 +0100

    Enhancement of userDetails function.
    The function userDetails() to always return the user details in the order specified in type User.

--- fileFunction.test.ts
+++ fileFunction.test.ts
@@ -16,3 +16,19 @@ it ('should return the details of type user joined with a vertical bar ', () => 
     expect(output).toBe('1|DS123|Dummy|User|DS12345')
 });
 
+
+it ('should return the user details in the exact order as listed in type User ', () => {
+    const testUser: User = {
+        studentId: "DS12345",
+        userName: "DS123",
+        id: 1,
+        firstName: "Dummy",
+        lastName: "User"
+    }
+
+    const output = userDetails(testUser)
+
+    expect(output).toBe('1|DS123|Dummy|User|DS12345')
+
+});
+
--- fileFunction.ts
+++ fileFunction.ts
@@ -4,7 +4,9 @@ import {User} from "./typeDeclaration";
 export const userDetails = (user: User): string => {
 
     const studentDetails: Array<string | number>= [];
-    for (let key in user) {
+    const keys: Array<keyof User> = ["id", "userName", "firstName", "lastName", "studentId"]
+
+    for (let key of keys) {
         if(user.hasOwnProperty(key)) {
             studentDetails.push(user[key as keyof User])
         }

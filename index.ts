3cae6966db40f4513042fdae5ad3e12bb1f0f35f
MaidaMujagic <maida.mujagic@york.ac.uk>
2023-09-20 11:36:37 +0100

    Set up mocks for printUserDetails()

--- index.ts
+++ index.ts
@@ -1,13 +1,6 @@
 import {userDetails} from "./functions";
 import {User} from "./types";
 
-const student: User = {
-    id: 1,
-    userName: "JS123",
-    firstName: "John",
-    lastName: "Smith",
-    studentId: "JS12345"
-}
 
 export const printUserDetails = (user: User) => {
     console.log(userDetails(user))

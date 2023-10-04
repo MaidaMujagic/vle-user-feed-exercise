5de02db9bb4edc5b270004c1603683bfafefaf33
MaidaMujagic <maida.mujagic@york.ac.uk>
2023-10-04 12:30:11 +0100

    Add config file for XO
    Added config file for XO to ensure that prettier & XO do not conflict, as well some additional rules
    
    Added .prettierignore to ensure that certain files are not formatted when running the format script.
    
    Run the format script to ensure that all .ts files conform to prettier standards.

--- functions.test.ts
+++ functions.test.ts
@@ -1,29 +1,29 @@
-import {expect, it, describe} from 'vitest';
-import {User} from "./types";
-import {userDetails} from "./functions";
+import { expect, it } from "vitest";
+import type { User } from "./types.js";
+import { userDetails } from "./functions.js";
 
+it("should return the user details in the same sequence as outlined in type User and joined with a vertical bar", () => {
+  const testUsers: User[] = [
+    {
+      studentId: "DS12345",
+      id: 1,
+      userName: "DS123",
+      firstName: "Dummy",
+      lastName: "User",
+    },
+    {
+      firstName: "Test",
+      lastName: "User",
+      id: 2,
+      userName: "TS123",
+      studentId: "TS12345",
+    },
+  ];
 
-it ('should return the user details in the same sequence as outlined in type User and joined with a vertical bar', () => {
-    const testUsers: User []= [
-        {
-            studentId: "DS12345",
-            id: 1,
-            userName: "DS123",
-            firstName: "Dummy",
-            lastName: "User"
+  const output = userDetails(testUsers);
 
-        },
-        {
-            firstName: "Test",
-            lastName: "User",
-            id: 2,
-            userName: "TS123",
-            studentId: "TS12345"
-        },
-    ]
-
-    const output = userDetails(testUsers)
-
-    expect(output).toStrictEqual(['1|DS123|Dummy|User|DS12345','2|TS123|Test|User|TS12345'])
-
+  expect(output).toStrictEqual([
+    "1|DS123|Dummy|User|DS12345",
+    "2|TS123|Test|User|TS12345",
+  ]);
 });

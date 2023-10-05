454b6e9c12f0a67844c0c2f2c273195b10e8b3cb
MaidaMujagic <maida.mujagic@york.ac.uk>
2023-10-05 12:02:09 +0100

    Move printUserDetails() to print-users.ts
    Refactor index.ts by moving the printUserDetails() into a new file print-users.ts & removing the optional chaining operator.
    
    Rename index.test.ts to print-users.ts to match the new file.
    
    Ran checkformat then format to ensure all files adhere to prettier guidelines.

---
+++ print-users.ts
@@ -0,0 +1,12 @@
+import type { User } from "./types";
+import { userDetails } from "./functions.js";
+
+export const printUserDetails = (users: User[]) => {
+  const userOutputs = userDetails(users);
+  const consolidatedOutput =
+    "EXTERNAL_PERSON_KEY|USER_ID|FIRSTNAME|LASTNAME|STUDENT_ID" +
+    "\n" +
+    userOutputs.join("\n");
+
+  console.log(consolidatedOutput);
+};

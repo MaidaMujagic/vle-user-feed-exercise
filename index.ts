904c9564a41739920b25f167f2d940d9e2eef997
MaidaMujagic <maida.mujagic@york.ac.uk>
2023-10-16 14:09:52 +0100

    Change signature of outputFetchedData()
    Rename the () in the index.ts file to outputFetchedData and changed its signature to async/await.
    
    Also, uninstalled libraries tha tare not used.

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
 import { printUserDetails } from "./print-users.js";
 import { fetchData } from "./fetch-data.js";
 
-const data = () => {
-  fetchData()
-    .then((result) => {
-      printUserDetails(result);
-    })
-    .catch((error) => {
+const outputFetchedData = async () => {
+    try {
+        const result = await fetchData();
+        printUserDetails(result)
+    } catch (error) {
       console.error("Error printing data:", error);
-    });
+    }
 };
 
-data();
+void outputFetchedData();

30a5523d643e598ea2164ea160b1724a3f053780
MaidaMujagic <maida.mujagic@york.ac.uk>
2023-10-23 11:33:59 +0100

    Move outputFetchedData() into print-users.ts
    Moved outputFetchedData() into print-users.ts
    
    Declared printUserDetails() as private.
    
    Added tests to verify behaviour of the outputFetchedData().

--- index.ts
+++ index.ts
@@ -1,13 +1,3 @@
-import { printUserDetails } from "./print-users.js";
-import { fetchData } from "./fetch-data.js";
+import { outputFetchedData } from "./print-users.js";
 
-const outputFetchedData = async () => {
-  try {
-    const result = await fetchData();
-    printUserDetails(result);
-  } catch (error) {
-    console.error("Error printing data:", error);
-  }
-};
-
-void outputFetchedData();
+await outputFetchedData();
--- print-users.ts
+++ print-users.ts
@@ -1,7 +1,8 @@
 import type { User } from "./types.js";
 import { userDetails } from "./functions.js";
+import { fetchData } from "./fetch-data.js";
 
-export const printUserDetails = (users: User[]) => {
+const printUserDetails = (users: User[]) => {
   const userOutputs = userDetails(users);
   const consolidatedOutput =
     "EXTERNAL_PERSON_KEY|USER_ID|FIRSTNAME|LASTNAME|STUDENT_ID" +
@@ -9,4 +10,13 @@ export const printUserDetails = (users: User[]) => {
     userOutputs.join("\n");
 
   console.log(consolidatedOutput);
+};
+
+export const outputFetchedData = async () => {
+  try {
+    const result = await fetchData();
+    printUserDetails(result);
+  } catch (error) {
+    console.error("Error printing data:", error);
+  }
 };
